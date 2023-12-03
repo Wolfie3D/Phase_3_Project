bb45f5b1dec2fcd866e10c5f9cb55924f340d138
Chris Kamau <kwangechi3@gmail.com>
2023-12-02 20:38:26 -0800

    Deployement

--- app.py
+++ app.py
@@ -1,27 +1,26 @@
 from flask import Flask, render_template, request
-from joblib import load
+import joblib 
 import pandas as pd
 
 app = Flask(__name__)
 
 # Load the ETL pipeline and the trained RandomForestRegressor model
-rf_model = load('/MODELS/lr_model.joblib')
+rf_model = joblib.load('MODELS\\lr_model.joblib')
 
 
-@app.route('/index.html')
+@app.route('/')
 def index():
     return render_template('index.html')
 
 
-@app.route('/result.html', methods=['POST'])
+@app.route('/Result', methods=['POST'])
 def predictions():
     if request.method == 'POST':
-
         # Collect user input from the form
         County = request.form['County']
         Neighborhood = request.form['Neighborhood']
-        Bedrooms = request.form['Bedrooms']
-        Bathrooms = request.form['Bathrooms']
+        bedrooms = request.form['bedrooms']
+        bathrooms = request.form['bathrooms']
         Size = request.form['Size']
         Quality = request.form['Quality']
         Type = request.form['Type']
@@ -31,19 +30,18 @@ def predictions():
         user_data = pd.DataFrame({
             'County': [County],
             'Neighborhood': [Neighborhood],
-            'Bedrooms': [Bedrooms],
-            'Bathrooms': [Bathrooms],
+            'bedrooms': [bedrooms],
+            'bathrooms': [bathrooms],
             'Size': [Size],
             'Quality': [Quality],
             'Type': [Type],
             'Furnished': [Furnished],
         })
 
-        # Make prediction using the RandomForestRegressor model
-        prediction = rf_model.predict(user_data)[0]
+        prediction = rf_model.predict(user_data)
 
         # Display the prediction to the user
-        return render_template('/templates/result.html', prediction=prediction)
+        return render_template('result.html', prediction=prediction)
 
 
 if __name__ == '__main__':
